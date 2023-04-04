38a108382a55699e6c24a30d12ef9e8f4a9c633e
burningcherry <pmarecki4@gmail.com>
2023-04-05 00:35:58 +0200

    Day 14

---
+++ 14/P1/program.cpp
@@ -0,0 +1,126 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <tuple>
+
+using namespace std;
+
+bool stable(vector<vector<char>>& map, int x, int y) {
+	return map[y+1][x-1] != '.' && map[y+1][x] != '.' && map[y+1][x+1] != '.';
+}
+
+pair<int,int> move_sand(vector<vector<char>>& map, int x, int y) {
+	map[y][x] = '.';
+	for (int i : {0,-1,1}) {
+		if (map[y+1][x+i] == '.') {
+			map[y+1][x+i] = 'o';
+			return {y+1, x+i};
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	fstream in{"input.txt"};
+	string line;
+
+	vector<vector<pair<int,int>>> labirynth{};
+	while (getline(in, line)) {
+		vector<pair<int,int>> path{};
+		int x, y;
+		char comma;
+		string dummy;
+		istringstream iss{line};
+		pair<int,int> start{};
+		iss >> start.second >> comma >> start.first;
+		path.push_back(start);
+		while (!iss.eof()) {
+			pair<int,int> node{};
+			iss >> dummy >> node.second >> comma >> node.first;
+			path.push_back(node);
+		}
+		labirynth.push_back(path);
+	}
+
+	int x_min = INT32_MAX, x_max = INT32_MIN, y_min = 0, y_max = INT32_MIN;
+	for (auto& a : labirynth) {
+		for (auto& b : a) {
+			x_min = min(x_min, b.second);
+			x_max = max(x_max, b.second);
+			y_max = max(y_max, b.first);
+		}
+	}
+
+	x_min -= 2;
+	int source = 500 - x_min;
+
+	for (auto& a : labirynth) {
+		for (auto& b : a) {
+			b.second -= x_min;
+		}
+	}
+	x_max -= x_min;
+	x_min = 0;
+	// cout << x_min << ' ' << x_max << ' ' << y_min << ' ' << y_max << endl;
+
+	vector<vector<char>> map(y_max - y_min + 2, vector<char>(x_max - x_min + 3, '.'));
+
+	for (auto& a: labirynth) {
+		int y = a.front().first;
+		int x = a.front().second;
+		map[y][x] = '#';
+		int xx, yy;
+		for (int i = 1; i < a.size(); ++i) {
+			if (a[i].second != x) {
+				xx = a[i].second;
+				for (int j = x; ; j += (xx > x ? 1 : -1)) {
+					map[y][j] = '#';
+					if (j == xx)
+						break;
+				}
+				x = xx;
+			} else {
+				yy = a[i].first;
+				for (int j = y; ; j += (yy > y ? 1 : -1)) {
+					map[j][x] = '#';
+					if (j == yy)
+						break;
+				}
+				y = yy;
+			}
+		}
+	}
+
+	map[0][source] = '+';
+
+	int grains = 0;
+	for (bool success = true; success; ++grains) {
+	// for (int c = 0; c < 5; ++c) {
+		int x = source;
+		int y = 1;
+		map[y][x] = 'o';
+		while (!stable(map, x, y)) {
+			std::tie(y, x) = move_sand(map, x, y);
+			if (y > y_max) {
+				success = false;
+				map[y][x] = '.';
+				break;
+			}
+		}
+	}
+	--grains;
+
+	// for (int i = 0; i < map.size(); ++i) {
+	// 	for (int j = 0; j < map.front().size(); ++j) {
+	// 		cout << map[i][j];
+	// 	}
+	// 	cout << endl;
+	// }
+
+	cout << grains << endl;
+
+	return 0;
+}
